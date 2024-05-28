6ffe83d777abeaa3cb3a3b861e9f99db8ea6d2af
pelhombre <elhombepol120@gmail.com>
2024-05-28 19:50:47 +0200

    services param

--- src/services/noteServices.js
+++ src/services/noteServices.js
@@ -1,32 +1,44 @@
+import { http } from "./http";
+
 const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 const URL_PATH = "notes";
 
-const URL_TO_FETCH = `${BASE_URL}/${URL_PATH}`;
-
 const getNotes = () => {
-  return fetch(URL_TO_FETCH)
-    .then((response) => response.json())
-    .then((data) => data.notes);
+  return http.get({
+    url: BASE_URL,
+    resourcePath: URL_PATH,
+  });
 };
 
 const deleteNote = (noteID) => {
-  return fetch(`${URL_TO_FETCH}/${noteID}`, { method: "DELETE" });
-};
-
-const updateNote = (updatingNote) => {
-  return fetch(URL_TO_FETCH, {
-    method: "PUT",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(updatingNote),
+  return http.delete({
+    url: BASE_URL,
+    resourcePath: `${URL_PATH}/${noteID}`,
   });
 };
 
 const createNote = (newNote) => {
-  return fetch(URL_TO_FETCH, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(newNote),
+  return http.create({
+    url: BASE_URL,
+    resourcePath: URL_PATH,
+    config: {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(newNote),
+    },
+  });
+};
+
+const updateNote = (modifiedNote) => {
+  return http.update({
+    url: BASE_URL,
+    resourcePath: `${URL_PATH}/${modifiedNote.id}`,
+    config: {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(modifiedNote),
+    },
   });
 };
 
